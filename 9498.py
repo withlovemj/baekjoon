1899d8dab5eee0fc9eb14b057349080c6afa5869
withlovemj <78516860+withlovemj@users.noreply.github.com>
2021-02-05 21:55:54 +0900

    Create 9498.py

---
+++ 9498.py
@@ -0,0 +1,16 @@
+# 백준 9498번
+# 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램 만들기
+
+
+score = int(input())
+
+if score >= 90:
+    print('A')
+elif score >= 80:
+    print('B')
+elif score >= 70:
+    print('C')
+elif score >= 60:
+    print('D')
+else:
+    print('F')
